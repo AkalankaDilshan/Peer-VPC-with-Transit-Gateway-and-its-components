d60833b6cf94c1718be91418e49ef58b8c9cc9fe
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-03 15:54:07 +0530

    feat: logically set sg for suite private ec2 & public ec2

--- modules/ec2-sg/variables.tf
+++ modules/ec2-sg/variables.tf
@@ -7,3 +7,15 @@ variable "security_group_name" {
   description = "name for web sg"
   type        = string
 }
+
+variable "is_allow_http" {
+  description = "is allow HTTP rule for EC2 security group"
+  type        = bool
+  default     = true
+}
+
+variable "is_allow_https" {
+  description = "is allow HTTPS rule for EC2 security group"
+  type        = bool
+  default     = true
+}
