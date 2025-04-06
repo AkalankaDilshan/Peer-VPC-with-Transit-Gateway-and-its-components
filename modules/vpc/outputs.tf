9e996b595d9cdb076fac338ab22fb9153fc8b218
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-06 13:54:33 +0530

    feat: add necessary output values

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -6,10 +6,23 @@ output "vpc_name" {
   value = var.vpc_name
 }
 
+output "vpc_cidr" {
+  value = aws_vpc.main_vpc.cidr_block
+}
+
+
 output "private_subnet_id" {
   value = aws_subnet.private_subnet.id
 }
 
 output "public_subnet_id" {
   value = aws_subnet.public_subnet.id
+}
+
+output "public_rt_id" {
+  value = aws_route_table.public_route.id
+}
+
+output "private_rt_id" {
+  value = aws_route_table.private_rt.id
 }
