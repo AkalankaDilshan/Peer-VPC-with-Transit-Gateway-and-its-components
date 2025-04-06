93cd8e5bfd8bc98fd48cb5990d50e50b62b7c2f3
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-06 09:00:28 +0530

    fix: comment provider conflict

--- main.tf
+++ main.tf
@@ -4,32 +4,32 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-provider "aws" {
-  alias = "eu-south-1"
+# provider "aws" {
+#   alias = "eu-south-1"
 
-  region = "eu-south-1"
-}
+#   region = "eu-south-1"
+# }
 
 module "vpc_A" {
-  source     = "./modules/vpc"
-  providers = {
-    aws = aws.eu-west-1
-  }
+  source = "./modules/vpc"
+  # providers = {
+  #   aws = aws.eu-west-1
+  # }
   vpc_name   = "VPC-A"
   cidr_block = "192.173.0.0/16"
   # aws_region           = "eu-north-1" // default region 
-  availability_zones   = "eu-north-1a"
+  availability_zones   = "eu-west-1a"
   public_subnet_cidr   = "192.173.1.0/24"
   private_subnet_cidrs = "192.173.3.0/24"
 }
 
 module "vpc_B" {
-  source     = "./modules/vpc"
-  providers  = { aws = aws.region2 }
+  source = "./modules/vpc"
+  # providers  = { aws = aws.region2 }
   vpc_name   = "VPC-B"
   cidr_block = "172.16.0.0/16"
   # aws_region           = "eu-west-1"
-  availability_zones   = "eu-west-1a"
+  availability_zones   = "eu-west-1b"
   public_subnet_cidr   = "172.16.1.0/24"
   private_subnet_cidrs = "172.16.3.0/24"
 }
