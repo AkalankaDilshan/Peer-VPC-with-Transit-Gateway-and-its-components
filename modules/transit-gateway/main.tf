7b21615e42dc8dd047b86ee35d9575e6eebd9ad3
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-06 14:32:57 +0530

    fix : add depend on feature

--- modules/transit-gateway/main.tf
+++ modules/transit-gateway/main.tf
@@ -33,12 +33,20 @@ resource "aws_route" "vpc_a_to_tgw_public_rt" {
   route_table_id         = var.vpc_a_public_rt_id
   destination_cidr_block = var.vpc_b_cidr
   transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
+
+  depends_on = [aws_ec2_transit_gateway.tgw,
+    aws_ec2_transit_gateway_vpc_attachment.VPC_A_attachment,
+  aws_ec2_transit_gateway_vpc_attachment.VPC_B_attachment]
 }
 
 resource "aws_route" "vpc_a_to_tgw_private_rt" {
   route_table_id         = var.vpc_a_private_rt_id
   destination_cidr_block = var.vpc_b_cidr
   transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
+
+  depends_on = [aws_ec2_transit_gateway.tgw,
+    aws_ec2_transit_gateway_vpc_attachment.VPC_A_attachment,
+  aws_ec2_transit_gateway_vpc_attachment.VPC_B_attachment]
 }
 
 # Add Routes to vpc B public and private rt
@@ -47,10 +55,18 @@ resource "aws_route" "vpc_b_to_tgw_public_rt" {
   route_table_id         = var.vpc_b_public_rt_id
   destination_cidr_block = var.vpc_a_cidr
   transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
+
+  depends_on = [aws_ec2_transit_gateway.tgw,
+    aws_ec2_transit_gateway_vpc_attachment.VPC_A_attachment,
+  aws_ec2_transit_gateway_vpc_attachment.VPC_B_attachment]
 }
 
 resource "aws_route" "vpc_b_to_tgw_private_rt" {
   route_table_id         = var.vpc_b_private_rt_id
   destination_cidr_block = var.vpc_a_cidr
   transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
+
+  depends_on = [aws_ec2_transit_gateway.tgw,
+    aws_ec2_transit_gateway_vpc_attachment.VPC_A_attachment,
+  aws_ec2_transit_gateway_vpc_attachment.VPC_B_attachment]
 }
