b28339692b944e429eba1852d4fe20bea7d4ded6
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-06 13:58:31 +0530

    feat: set some tgw id outputs

---
+++ modules/transit-gateway/outputs.tf
@@ -0,0 +1,13 @@
+# Output the Transit Gateway ID
+output "transit_gateway_id" {
+  value = aws_ec2_transit_gateway.tgw.id
+}
+
+# Output the Attachment IDs
+output "vpc_a_attachment_id" {
+  value = aws_ec2_transit_gateway_vpc_attachment.VPC_A_attachment.id
+}
+
+output "vpc_b_attachment_id" {
+  value = aws_ec2_transit_gateway_vpc_attachment.VPC_B_attachment.id
+}
