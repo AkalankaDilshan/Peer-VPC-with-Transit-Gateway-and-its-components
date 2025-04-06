1f7510dc860a0bc1554b253fdaab03e18bd1f28d
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-06 13:45:42 +0530

    feat: roughly complete transit gw and routes

---
+++ modules/transit-gateway/variables.tf
@@ -0,0 +1,48 @@
+variable "vpc_a_id" {
+  description = "public vpc ids"
+  type        = string
+}
+
+variable "vpc_b_id" {
+  description = "private vpc ids"
+  type        = string
+}
+
+variable "vpc_a_cidr" {
+  description = "for set route"
+  type        = string
+}
+
+variable "vpc_b_cidr" {
+  description = "for set route"
+  type        = string
+}
+variable "vpc_a_subnet_ids" {
+  description = "public and private subnet ids in first vpc"
+  type        = list(string)
+}
+
+variable "vpc_b_subnet_ids" {
+  description = "public and private subnet ids in first vpc"
+  type        = list(string)
+}
+
+variable "vpc_a_public_rt_id" {
+  description = "for set tgw route"
+  type        = string
+}
+
+variable "vpc_a_private_rt_id" {
+  description = "for set tgw route"
+  type        = string
+}
+
+variable "vpc_b_public_rt_id" {
+  description = "for set tgw route"
+  type        = string
+}
+
+variable "vpc_b_private_rt_id" {
+  description = "for set tgw route"
+  type        = string
+}
