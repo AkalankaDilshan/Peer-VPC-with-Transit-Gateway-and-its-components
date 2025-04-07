3605328a1b2346986388f336362ec98c5fdee5e5
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-07 16:07:37 +0530

    refactor : comment some function for testing purpopse

--- modules/transit-gateway/main.tf
+++ modules/transit-gateway/main.tf
@@ -2,71 +2,70 @@ resource "aws_ec2_transit_gateway" "tgw" {
   description                     = "Transit Gateway for connect VPC A and VPC B"
   default_route_table_association = "enable"
   default_route_table_propagation = "enable"
-
   tags = {
-    Name = "main-tgw"
+    Name = var.gateway_name
   }
 }
 
 # attachment for vpc a 
-resource "aws_ec2_transit_gateway_vpc_attachment" "VPC_A_attachment" {
-  subnet_ids         = var.vpc_a_subnet_ids
-  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
-  vpc_id             = var.vpc_a_id
-  tags = {
-    Name = "tgw-vpc-a-attachment"
-  }
-}
+# resource "aws_ec2_transit_gateway_vpc_attachment" "VPC_A_attachment" {
+#   subnet_ids         = var.vpc_a_subnet_ids
+#   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+#   vpc_id             = var.vpc_a_id
+#   tags = {
+#     Name = "tgw-vpc-a-attachment"
+#   }
+# }
 
-# attachment for vpc b
-resource "aws_ec2_transit_gateway_vpc_attachment" "VPC_B_attachment" {
-  subnet_ids         = var.vpc_b_subnet_ids
-  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
-  vpc_id             = var.vpc_b_id
-  tags = {
-    Name = "tgw-vpc-b-attachment"
-  }
-}
+# # attachment for vpc b
+# resource "aws_ec2_transit_gateway_vpc_attachment" "VPC_B_attachment" {
+#   subnet_ids         = var.vpc_b_subnet_ids
+#   transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+#   vpc_id             = var.vpc_b_id
+#   tags = {
+#     Name = "tgw-vpc-b-attachment"
+#   }
+# }
 
-# Add Routes to vpc A public and private rt 
-resource "aws_route" "vpc_a_to_tgw_public_rt" {
-  route_table_id         = var.vpc_a_public_rt_id
-  destination_cidr_block = var.vpc_b_cidr
-  transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
+# # Add Routes to vpc A public and private rt 
+# resource "aws_route" "vpc_a_to_tgw_public_rt" {
+#   route_table_id         = var.vpc_a_public_rt_id
+#   destination_cidr_block = var.vpc_b_cidr
+#   transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
 
-  depends_on = [aws_ec2_transit_gateway.tgw,
-    aws_ec2_transit_gateway_vpc_attachment.VPC_A_attachment,
-  aws_ec2_transit_gateway_vpc_attachment.VPC_B_attachment]
-}
+#   depends_on = [aws_ec2_transit_gateway.tgw,
+#     aws_ec2_transit_gateway_vpc_attachment.VPC_A_attachment,
+#   aws_ec2_transit_gateway_vpc_attachment.VPC_B_attachment]
+# }
 
-resource "aws_route" "vpc_a_to_tgw_private_rt" {
-  route_table_id         = var.vpc_a_private_rt_id
-  destination_cidr_block = var.vpc_b_cidr
-  transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
+# resource "aws_route" "vpc_a_to_tgw_private_rt" {
+#   route_table_id         = var.vpc_a_private_rt_id
+#   destination_cidr_block = var.vpc_b_cidr
+#   transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
 
-  depends_on = [aws_ec2_transit_gateway.tgw,
-    aws_ec2_transit_gateway_vpc_attachment.VPC_A_attachment,
-  aws_ec2_transit_gateway_vpc_attachment.VPC_B_attachment]
-}
+#   depends_on = [aws_ec2_transit_gateway.tgw,
+#     aws_ec2_transit_gateway_vpc_attachment.VPC_A_attachment,
+#   aws_ec2_transit_gateway_vpc_attachment.VPC_B_attachment]
+# }
 
-# Add Routes to vpc B public and private rt
+# # Add Routes to vpc B public and private rt
 
-resource "aws_route" "vpc_b_to_tgw_public_rt" {
-  route_table_id         = var.vpc_b_public_rt_id
-  destination_cidr_block = var.vpc_a_cidr
-  transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
+# resource "aws_route" "vpc_b_to_tgw_public_rt" {
+#   route_table_id         = var.vpc_b_public_rt_id
+#   destination_cidr_block = var.vpc_a_cidr
+#   transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
 
-  depends_on = [aws_ec2_transit_gateway.tgw,
-    aws_ec2_transit_gateway_vpc_attachment.VPC_A_attachment,
-  aws_ec2_transit_gateway_vpc_attachment.VPC_B_attachment]
-}
+#   depends_on = [aws_ec2_transit_gateway.tgw,
+#     aws_ec2_transit_gateway_vpc_attachment.VPC_A_attachment,
+#   aws_ec2_transit_gateway_vpc_attachment.VPC_B_attachment]
+# }
 
-resource "aws_route" "vpc_b_to_tgw_private_rt" {
-  route_table_id         = var.vpc_b_private_rt_id
-  destination_cidr_block = var.vpc_a_cidr
-  transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
+# resource "aws_route" "vpc_b_to_tgw_private_rt" {
+#   route_table_id         = var.vpc_b_private_rt_id
+#   destination_cidr_block = var.vpc_a_cidr
+#   transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
 
-  depends_on = [aws_ec2_transit_gateway.tgw,
-    aws_ec2_transit_gateway_vpc_attachment.VPC_A_attachment,
-  aws_ec2_transit_gateway_vpc_attachment.VPC_B_attachment]
-}
+#   depends_on = [aws_ec2_transit_gateway.tgw,
+#     aws_ec2_transit_gateway_vpc_attachment.VPC_A_attachment,
+#   aws_ec2_transit_gateway_vpc_attachment.VPC_B_attachment]
+# }
--- modules/transit-gateway/variables.tf
+++ modules/transit-gateway/variables.tf
@@ -1,48 +1,53 @@
-variable "vpc_a_id" {
-  description = "public vpc ids"
+variable "gateway_name" {
+  description = "name for transit gateway"
   type        = string
 }
 
-variable "vpc_b_id" {
-  description = "private vpc ids"
-  type        = string
-}
+# variable "vpc_a_id" {
+#   description = "public vpc ids"
+#   type        = string
+# }
 
-variable "vpc_a_cidr" {
-  description = "for set route"
-  type        = string
-}
+# variable "vpc_b_id" {
+#   description = "private vpc ids"
+#   type        = string
+# }
 
-variable "vpc_b_cidr" {
-  description = "for set route"
-  type        = string
-}
-variable "vpc_a_subnet_ids" {
-  description = "public and private subnet ids in first vpc"
-  type        = list(string)
-}
+# variable "vpc_a_cidr" {
+#   description = "for set route"
+#   type        = string
+# }
 
-variable "vpc_b_subnet_ids" {
-  description = "public and private subnet ids in first vpc"
-  type        = list(string)
-}
+# variable "vpc_b_cidr" {
+#   description = "for set route"
+#   type        = string
+# }
+# variable "vpc_a_subnet_ids" {
+#   description = "public and private subnet ids in first vpc"
+#   type        = list(string)
+# }
 
-variable "vpc_a_public_rt_id" {
-  description = "for set tgw route"
-  type        = string
-}
+# variable "vpc_b_subnet_ids" {
+#   description = "public and private subnet ids in first vpc"
+#   type        = list(string)
+# }
 
-variable "vpc_a_private_rt_id" {
-  description = "for set tgw route"
-  type        = string
-}
+# variable "vpc_a_public_rt_id" {
+#   description = "for set tgw route"
+#   type        = string
+# }
 
-variable "vpc_b_public_rt_id" {
-  description = "for set tgw route"
-  type        = string
-}
+# variable "vpc_a_private_rt_id" {
+#   description = "for set tgw route"
+#   type        = string
+# }
 
-variable "vpc_b_private_rt_id" {
-  description = "for set tgw route"
-  type        = string
-}
+# variable "vpc_b_public_rt_id" {
+#   description = "for set tgw route"
+#   type        = string
+# }
+
+# variable "vpc_b_private_rt_id" {
+#   description = "for set tgw route"
+#   type        = string
+# }
