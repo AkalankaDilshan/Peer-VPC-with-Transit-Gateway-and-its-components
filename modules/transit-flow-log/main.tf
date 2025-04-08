a07acbed2f3657a6a370f49ecfc889e797d75b3b
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-08 16:46:04 +0530

    fix : not support resource type

--- modules/transit-flow-log/main.tf
+++ modules/transit-flow-log/main.tf
@@ -1,14 +1,23 @@
-resource "aws_ec2_transit_gateway_flow_log" "tgw_flow_log_cloudwatch" {
+resource "aws_flow_log" "tgw_flow_log_cloudwatch" {
   transit_gateway_id = var.transit_gateway_id
   # transit_gateway_attachment_id = var.transit_gateway_attachment_id
-  log_destination = var.cloudwatch_log_group_arn
-  iam_role_arn    = var.iam_role_arn
-  traffic_type    = "ALL"
+  log_destination      = var.cloudwatch_log_group_arn
+  log_destination_type = "cloud-watch-logs"
+  iam_role_arn         = var.iam_role_arn
+  traffic_type         = "ALL"
+  tags = {
+    Name = "TGWFlowLogToCloudWatch"
+  }
 }
 
-resource "aws_ec2_transit_gateway_flow_log" "tgw_flow_log_s3" {
+resource "aws_flow_log" "tgw_flow_log_s3" {
   transit_gateway_id = var.transit_gateway_id
   # transit_gateway_attachment_id = var.transit_gateway_attachment_id
-  log_destination = var.s3_bucket_arn
-  traffic_type    = "ALL"
+  log_destination      = var.s3_bucket_arn
+  log_destination_type = "s3"
+  iam_role_arn         = var.iam_role_arn
+  traffic_type         = "ALL"
+  tags = {
+    Name = "TGWFlowLogToS3"
+  }
 }
