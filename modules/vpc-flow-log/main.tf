563dfc4098125013cb1f08af00f6941aa682de6f
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-08 16:49:03 +0530

    fix : list type error :accordion:

--- modules/vpc-flow-log/main.tf
+++ modules/vpc-flow-log/main.tf
@@ -1,5 +1,5 @@
 resource "aws_flow_log" "vpc_flow_log_cloudwatch" {
-  for_each             = toset(var.vpc_ids)
+  for_each             = toset([var.vpc_ids])
   log_destination_type = "cloud-watch-logs"
   log_destination      = var.cloudwatch_log_group_arn
   iam_role_arn         = var.iam_role_arn
@@ -8,7 +8,7 @@ resource "aws_flow_log" "vpc_flow_log_cloudwatch" {
 }
 
 resource "aws_flow_log" "vpc_flow_log_s3" {
-  for_each             = toset(var.vpc_ids)
+  for_each             = toset([var.vpc_ids])
   log_destination_type = "s3"
   log_destination      = "${var.s3_bucket_arn}/prefix"
   iam_role_arn         = var.iam_role_arn
