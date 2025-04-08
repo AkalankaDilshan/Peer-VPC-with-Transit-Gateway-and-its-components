abab17f7df115cd7239db889657351719e629754
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-08 15:01:37 +0530

    feat: roughly complete vpc flow log iam role

---
+++ modules/vpc-flow-log-iam-role/main.tf
@@ -0,0 +1,47 @@
+# iam role for vpc-flow logs 
+resource "aws_iam_role" "vpc_flow_log_role" {
+  name = var.role_name
+
+  assume_role_policy = jsondecode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "vpc-flow-logs.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# create Iam role police data 
+data "aws_iam_policy_document" "polices" {
+  version = "2012-10-17"
+
+  # allow log group 
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "custom_policy" {
+  name        = "vpc_flow_log_policy"
+  description = "Policy for vpc flow log role"
+  policy      = data.aws_iam_policy_document.polices.json
+}
+
+# Attach policy to Iam role 
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.vpc_flow_log_role.name
+  policy_arn = aws_iam_policy.custom_policy.arn
+}
---
+++ modules/vpc-flow-log-iam-role/variables.tf
@@ -0,0 +1,4 @@
+variable "role_name" {
+  description = "name for iam role"
+  type        = string
+}
