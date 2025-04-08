d13ec4ff97f27526f8cd9d552e75f450a52816e1
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-08 16:05:07 +0530

    feat: complete vpc flow log section

---
+++ modules/vpc-flow-log/main.tf
@@ -0,0 +1,17 @@
+resource "aws_flow_log" "vpc_flow_log_cloudwatch" {
+  for_each             = toset(var.vpc_ids)
+  log_destination_type = "cloud-watch-logs"
+  log_destination      = var.cloudwatch_log_group_arn
+  iam_role_arn         = var.iam_role_arn
+  traffic_type         = "ALL"
+  vpc_id               = each.value
+}
+
+resource "aws_flow_log" "vpc_flow_log_s3" {
+  for_each             = toset(var.vpc_ids)
+  log_destination_type = "s3"
+  log_destination      = "${var.s3_bucket_arn}/prefix"
+  iam_role_arn         = var.iam_role_arn
+  traffic_type         = "ALL"
+  vpc_id               = each.value
+}
---
+++ modules/vpc-flow-log/variables.tf
@@ -0,0 +1,19 @@
+variable "cloudwatch_log_group_arn" {
+  description = "log group arn"
+  type        = string
+}
+
+variable "iam_role_arn" {
+  description = "iam role arn for transit gateway work flow"
+  type        = string
+}
+
+variable "s3_bucket_arn" {
+  description = "for save flow logs"
+  type        = string
+}
+
+variable "vpc_ids" {
+  description = "all ids in vpcs"
+  type        = list(string)
+}
