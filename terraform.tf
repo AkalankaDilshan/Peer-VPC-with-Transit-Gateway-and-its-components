819e17cf9f915af8e75f4937d467c5a60d49d90d
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-08 16:37:21 +0530

    fix : older terraform version error

--- terraform.tf
+++ terraform.tf
@@ -2,9 +2,14 @@ terraform {
   cloud {
 
     organization = "ZeroCloud"
-
     workspaces {
       name = "Peer-VPC-with-Transit-Gateway-and-its-components"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
     }
   }
 }
