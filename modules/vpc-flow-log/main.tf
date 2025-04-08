595525715341af9e5c6c694c0275b76850b153e6
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-08 16:52:58 +0530

    fix : for each error

--- modules/vpc-flow-log/main.tf
+++ modules/vpc-flow-log/main.tf
@@ -1,17 +1,17 @@
 resource "aws_flow_log" "vpc_flow_log_cloudwatch" {
-  for_each             = toset([var.vpc_ids])
+  count                = length(var.vpc_ids)
   log_destination_type = "cloud-watch-logs"
   log_destination      = var.cloudwatch_log_group_arn
   iam_role_arn         = var.iam_role_arn
   traffic_type         = "ALL"
-  vpc_id               = each.value
+  vpc_id               = var.vpc_ids[count.index]
 }
 
 resource "aws_flow_log" "vpc_flow_log_s3" {
-  for_each             = toset([var.vpc_ids])
+  count                = length(var.vpc_ids)
   log_destination_type = "s3"
   log_destination      = "${var.s3_bucket_arn}/prefix"
   iam_role_arn         = var.iam_role_arn
   traffic_type         = "ALL"
-  vpc_id               = each.value
+  vpc_id               = var.vpc_ids[count.index]
 }
