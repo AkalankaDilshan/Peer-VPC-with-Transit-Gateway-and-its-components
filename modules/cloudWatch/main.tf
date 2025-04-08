3fff0178d00bafc4534e5fef7b37d00b18814b36
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-08 15:42:30 +0530

    feat: create basic cloudwatch log group

---
+++ modules/cloudWatch/main.tf
@@ -0,0 +1,5 @@
+# CloudWatch Log Group 
+resource "aws_cloudwatch_log_group" "log_group" {
+  name              = var.log_group_name
+  retention_in_days = 30
+}
---
+++ modules/cloudWatch/variables.tf
@@ -0,0 +1,4 @@
+variable "log_group_name" {
+  description = "name for cloudwatch log group"
+  type        = string
+}
