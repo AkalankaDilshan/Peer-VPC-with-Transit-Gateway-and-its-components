2cf0c6d759e3c44210fb8b2dc8845c65f0c0fad0
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-09 11:05:20 +0530

    feat: get log groups name output

--- modules/cloudWatch_logs/outputs.tf
+++ modules/cloudWatch_logs/outputs.tf
@@ -1,3 +1,7 @@
 output "cloudwatch_log_group_arn" {
   value = aws_cloudwatch_log_group.log_group.arn
 }
+
+output "cloudwatch_log_group_name" {
+  value = aws_cloudwatch_log_group.log_group.name
+}
