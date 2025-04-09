4df324faf09e0ef4e0245ec1668aa4b7b5627802
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-09 11:35:19 +0530

    fix : provider error

--- terraform.tf
+++ terraform.tf
@@ -1,15 +1,15 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
   cloud {
 
     organization = "ZeroCloud"
     workspaces {
       name = "Peer-VPC-with-Transit-Gateway-and-its-components"
-    }
-  }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
     }
   }
 }
