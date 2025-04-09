bb703991dfa2f543931ffe312be411418d8b16c0
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-09 11:49:54 +0530

    fix : try 1.2

--- terraform.tf
+++ terraform.tf
@@ -1,16 +1,5 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
   cloud {
-
     organization = "ZeroCloud"
     workspaces {
       name = "Peer-VPC-with-Transit-Gateway-and-its-components"
