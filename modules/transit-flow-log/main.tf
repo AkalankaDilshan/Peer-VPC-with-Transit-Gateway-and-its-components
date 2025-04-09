673295cbd3cf38803444b6155b9b67230ae3197f
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-04-09 12:28:38 +0530

    fix : max_aggregation_interval  error

--- modules/transit-flow-log/main.tf
+++ modules/transit-flow-log/main.tf
@@ -1,10 +1,11 @@
 resource "aws_flow_log" "tgw_flow_log_cloudwatch" {
   transit_gateway_id = var.transit_gateway_id
   # transit_gateway_attachment_id = var.transit_gateway_attachment_id
-  log_destination      = var.cloudwatch_log_group_arn
-  log_destination_type = "cloud-watch-logs"
-  iam_role_arn         = var.iam_role_arn
-  traffic_type         = "ALL"
+  log_destination          = var.cloudwatch_log_group_arn
+  log_destination_type     = "cloud-watch-logs"
+  iam_role_arn             = var.iam_role_arn
+  traffic_type             = "ALL"
+  max_aggregation_interval = 60
   tags = {
     Name = "TGWFlowLogToCloudWatch"
   }
@@ -13,10 +14,11 @@ resource "aws_flow_log" "tgw_flow_log_cloudwatch" {
 resource "aws_flow_log" "tgw_flow_log_s3" {
   transit_gateway_id = var.transit_gateway_id
   # transit_gateway_attachment_id = var.transit_gateway_attachment_id
-  log_destination      = var.s3_bucket_arn
-  log_destination_type = "s3"
-  iam_role_arn         = var.iam_role_arn
-  traffic_type         = "ALL"
+  log_destination          = var.s3_bucket_arn
+  log_destination_type     = "s3"
+  iam_role_arn             = var.iam_role_arn
+  traffic_type             = "ALL"
+  max_aggregation_interval = 60
   tags = {
     Name = "TGWFlowLogToS3"
   }
